949746a0d89976ad137ac4f2d0dc830c9bf03c7a
Julian Ortiz <36554107+Julian1022438899@users.noreply.github.com>
2022-10-06 15:35:13 -0500

    Proyecto inicial

---
+++ build/resources/test/features/academyChoucair.feature
@@ -0,0 +1,6 @@
+@stories
+  Feature: Academy Choucair
+    As a user I want to automate the utest.com page
+  @scenario1
+  Scenario: Register user
+    Given julian wants to register on the utest page to have a user
---
+++ src/test/java/co/com/choucair/certification/utest/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -0,0 +1,18 @@
+package co.com.choucair.certification.utest.stepdefinitions;
+
+import co.com.choucair.certification.utest.tasks.OpenUp;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class ChoucairAcademyStepDefinitions {
+    @Before
+    public void setStage (){
+        OnStage.setTheStage(new OnlineCast());
+    }
+    @Given("^julian wants to register on the utest page to have a user$")
+    public void julianWantsToRegisterOnTheUtestPageToHaveAUser() {
+        OnStage.theActorCalled("Julian").wasAbleTo(OpenUp.thePage());
+    }
+}
